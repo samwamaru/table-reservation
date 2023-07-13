f5966a9619497ac7f3cb4176555af3b24f92634e
jesse <jessebett1999@gmail.com>
2023-07-13 20:05:01 +0300

    render deploy

--- server/server.js
+++ server/server.js
@@ -11,12 +11,14 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-// app.use(
-//   cors({
-//     origin: 'https://table-mate.onrender.com/',
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: ["http://localhost:5173/", 
+    "https://table-mate.onrender.com"],
+    credentials: true,
+  })
+);
+
 app.use(cookieParser())
 // API routes
 app.use("/api", router);
