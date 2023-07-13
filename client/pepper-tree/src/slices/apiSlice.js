66b0403fad8ab6196057c758e69108557f5e82e1
jesse <jessebett1999@gmail.com>
2023-07-13 16:04:06 +0300

    second deployment test

--- client/pepper-tree/src/slices/apiSlice.js
+++ client/pepper-tree/src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:8080" });
+const baseQuery = fetchBaseQuery({ baseUrl: "https://table-mate-app-api.onrender.com" });
 
 export const apiSlice = createApi({
   baseQuery,
