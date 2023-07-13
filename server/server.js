e9e2f76a69bdb8b1f654bb1f29357f9507e117ea
jesse <jessebett1999@gmail.com>
2023-07-13 16:15:34 +0300

    changed origin url

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: 'https://table-mate-app-api.onrender.com',
+    origin: 'https://table-mate.onrender.com/',
     credentials: true,
   })
 );
