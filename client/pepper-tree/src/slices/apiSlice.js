a82ed1eb3a95c7f9baaef821d02413619c52687e
jesse <jessebett1999@gmail.com>
2023-12-20 10:16:59 +0300

    final

--- client/pepper-tree/src/slices/apiSlice.js
+++ client/pepper-tree/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-// const baseQuery = fetchBaseQuery({ baseUrl: "https://table-mate-app-api.onrender.com" });
-const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:8080" });
+const baseQuery = fetchBaseQuery({ baseUrl: "https://table-mate-app-api.onrender.com" });
+// const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:8080" });
 
 export const apiSlice = createApi({
   baseQuery,
